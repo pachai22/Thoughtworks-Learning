c6f6e7b945c7169732a802d5df52e93477b48f1b
K_Pachaiammal <pachai4477@gmail.com>
2020-06-10 07:29:29 +0100

    Added a second method using regular expression

--- obtain_the_domain_name.py
+++ obtain_the_domain_name.py
@@ -1,3 +1,5 @@
+#Type 1
+
 from urllib.parse import urlparse
 
 url=urlparse("https://www.hackerrank.com/domains/python")
@@ -7,3 +9,14 @@ print(url)
 print(url.netloc)
 
 print(url.netloc[4:])
+
+#Type 2
+
+import re
+
+url="https://www.hackerrank.com/domains/python"
+
+x=re.findall(r"www.(.*?)/d",url)
+
+print(*x)
+
