5ad9f6f99eedb9685b8e0cb65a18271c2c069f2a
K_Pachaiammal <pachai4477@gmail.com>
2020-06-12 06:33:07 +0100

    Added functionality for even number in pattern program

--- pattern_program.py
+++ pattern_program.py
@@ -1,11 +1,14 @@
 
 no_of_rows = int(input())
+if no_of_rows % 2 == 0:
+    print("Please enter the odd number")
+else:
 
-for i in range(1,no_of_rows,2):
-    print("*"*i)
+    for i in range(1,no_of_rows,2):
+        print("*"*i)
 
-for j in range(no_of_rows,0,-2):
-    print("*"*j)
+    for j in range(no_of_rows,0,-2):
+        print("*"*j)
 
 ''' Input : 5
 
