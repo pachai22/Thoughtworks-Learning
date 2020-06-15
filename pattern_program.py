55b2b99f8dcbb34834d5703ce020ed4646c20245
K_Pachaiammal <pachai4477@gmail.com>
2020-06-15 15:06:56 +0100

    Added a method using single for_loop in pattern program

--- pattern_program.py
+++ pattern_program.py
@@ -1,14 +1,33 @@
 
-no_of_rows = int(input())
-if no_of_rows % 2 == 0:
-    print("Please enter the odd number")
-else:
+#Type 1 (Using 2 for loop)
 
+no_of_rows = int(input("Enter the no of rows : "))
+if no_of_rows%2 != 0:
     for i in range(1,no_of_rows,2):
         print("*"*i)
 
     for j in range(no_of_rows,0,-2):
         print("*"*j)
+
+else:
+    print("Please enter the odd number of rows")
+print()
+
+#Type 2(Using 1 for loop)
+
+no_of_times=1
+if no_of_rows % 2 != 0:
+    symbol = input("Enter the symbol : ")
+    for i in range(1,no_of_rows+1):
+        if i < ((no_of_rows//2)+1):
+            print(symbol*no_of_times)
+            no_of_times+=2
+        else:
+            print(symbol*no_of_times)
+            no_of_times-=2
+else:
+    print("Please enter the odd number of rows !")
+            
 
 ''' Input : 5
 
