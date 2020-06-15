8652b5fe38dd9b6ab1f3e40f147921f9af204445
K_Pachaiammal <pachai4477@gmail.com>
2020-06-15 15:16:21 +0100

    Updated with giving user a choice to select continent

--- find_key_using_value_in_dictionary.py
+++ find_key_using_value_in_dictionary.py
@@ -1,10 +1,10 @@
 
 
 given_dictionary = {'Spain':'Europe','Japan':'Asia','India':'Asia','Italy':'Europe','Thailand':'Asia','Sudan':'Africa'}
-
+print("Please select any continent from this :",set(given_dictionary.values()))
 desired_continent = input("Desired continent :")
 
-
+print("The countries are :",end=" ")
 for key,value in given_dictionary.items():
     if value.lower() == desired_continent.lower() :
         print(key,end=" ")
