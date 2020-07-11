eaded3dbc1f77959228b1004f9f7e6301be5934b
Pachaiammal_k <pachai4477@gmail.com>
2020-07-11 21:02:00 +0530

    Exceptional handling assignment submission

---
+++ exceptional_handling_assignment.py
@@ -0,0 +1,13 @@
+
+while True:
+    try:
+        number=int(input("Enter the integer :"))
+    except:
+        
+        print("Please enter input as an integer again !")
+        continue
+    else:
+        print("Entered an integer")
+        break
+    
+        
