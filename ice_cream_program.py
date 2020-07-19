282b70421e2d8a2f76d3605c9265443f16c9d95e
Pachaiammal_k <pachai4477@gmail.com>
2020-07-19 09:40:06 +0530

    Updated to calculate for more than 1 quantity in ice cream program

--- ice_cream_program.py
+++ ice_cream_program.py
@@ -5,11 +5,12 @@ class IceCream:
     vanilla = 20
     chocolate = 40
     strawberry = 60
-    def __init__(self,flavour,shape):
+    def __init__(self,flavour,shape,quantity):
         self.flavour = flavour
         self.shape = shape
+        self.quantity = quantity
     def cost(self):
-        return getattr(ice_object,self.flavour) + getattr(ice_object,self.shape)
+        return (getattr(ice_object,self.flavour) + getattr(ice_object,self.shape))*self.quantity
         
 
 class Chocolate(IceCream):
@@ -17,12 +18,12 @@ class Chocolate(IceCream):
     caramel = 10
     nuts = 13
     no = 0
-    def __init__(self,flavour,shape):
-        super().__init__(flavour,shape)
+    def __init__(self,flavour,shape,quantity):
+        super().__init__(flavour,shape,quantity)
     def toppings(self):
         topping = input("\nChocolate flavour has topping options as chocochips :Rs 15,caramel :Rs 10,nuts :Rs 13 !\nChoose any one of the toppings mentioned above,Else type no : ")
         topping = topping.lower()
-        return getattr(ice_object,topping)
+        return (getattr(ice_object,topping))*self.quantity
             
 
 print("*-*-*Welcome to Green-icecream stall*-*-*")
@@ -30,7 +31,8 @@ print("*---------------MENU CARD---------------*\n")
 print("ICE CREAM          flavour+type    COST\nChocolate stick    40+5           Rs 45\nChocolate cone     40+10          Rs 50\nChocolate cup      40+15          Rs 55\nVanilla stick      20+5           Rs 25\nVanilla cone       20+10          Rs 30\nVanilla cup        20+15          Rs 35\nStrawberry stick   60+5           Rs 65\nStrawberry cone    60+10          Rs 70\nStrawberry cup     60+15          Rs 75")
 print("\nChoose your favourite ice-cream :",end=" ")
 choice = input().split()
-ice_object = Chocolate(choice[0].lower(),choice[1].lower())
+quantity = int(input("Please enter the quantity in number : "))
+ice_object = Chocolate(choice[0].lower(),choice[1].lower(),quantity)
 if choice[0].lower() != "chocolate":
     print("The cost of ice-cream is : Rs",ice_object.cost())
 else:
