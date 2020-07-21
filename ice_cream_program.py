b1ec15d9177ad7a6e382cfec3ed38d7659f3f625
Pachaiammal_k <pachai4477@gmail.com>
2020-07-21 10:48:45 +0530

    Added sample input and output,changed child class name

--- ice_cream_program.py
+++ ice_cream_program.py
@@ -13,7 +13,7 @@ class IceCream:
         return (getattr(ice_object,self.flavour) + getattr(ice_object,self.shape))*self.quantity
         
 
-class Chocolate(IceCream):
+class ChocolateIcecream(IceCream):
     chocochips = 15
     caramel = 10
     nuts = 13
@@ -32,8 +32,34 @@ print("ICE CREAM          flavour+type    COST\nChocolate stick    40+5         
 print("\nChoose your favourite ice-cream :",end=" ")
 choice = input().split()
 quantity = int(input("Please enter the quantity in number : "))
-ice_object = Chocolate(choice[0].lower(),choice[1].lower(),quantity)
+ice_object = ChocolateIcecream(choice[0].lower(),choice[1].lower(),quantity)
 if choice[0].lower() != "chocolate":
     print("The cost of ice-cream is : Rs",ice_object.cost())
 else:
     print("The cost of ice-cream is : Rs",ice_object.cost()+ice_object.toppings())
+
+"""
+Output :
+
+*-*-*Welcome to Green-icecream stall*-*-*
+*---------------MENU CARD---------------*
+
+ICE CREAM          flavour+type    COST
+Chocolate stick    40+5           Rs 45
+Chocolate cone     40+10          Rs 50
+Chocolate cup      40+15          Rs 55
+Vanilla stick      20+5           Rs 25
+Vanilla cone       20+10          Rs 30
+Vanilla cup        20+15          Rs 35
+Strawberry stick   60+5           Rs 65
+Strawberry cone    60+10          Rs 70
+Strawberry cup     60+15          Rs 75
+
+Choose your favourite ice-cream : chocolate cone
+Please enter the quantity in number : 3
+
+Chocolate flavour has topping options as chocochips :Rs 15,caramel :Rs 10,nuts :Rs 13 !
+Choose any one of the toppings mentioned above,Else type no : caramel
+The cost of ice-cream is : Rs 180
+
+"""
