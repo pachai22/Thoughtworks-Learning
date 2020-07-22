667010e92110ab4e890d7e563f96ac29302c8076
Pachaiammal_k <pachai4477@gmail.com>
2020-07-22 17:29:29 +0530

    Icecream program updated

--- ice_cream_program.py
+++ ice_cream_program.py
@@ -1,65 +1,81 @@
-class IceCream:
-    stick = 5
-    cone = 10
-    cup = 15
-    vanilla = 20
-    chocolate = 40
-    strawberry = 60
-    def __init__(self,flavour,shape,quantity):
-        self.flavour = flavour
-        self.shape = shape
-        self.quantity = quantity
-    def cost(self):
-        return (getattr(ice_object,self.flavour) + getattr(ice_object,self.shape))*self.quantity
-        
 
-class ChocolateIcecream(IceCream):
-    chocochips = 15
-    caramel = 10
-    nuts = 13
-    no = 0
-    def __init__(self,flavour,shape,quantity):
-        super().__init__(flavour,shape,quantity)
-    def toppings(self):
-        topping = input("\nChocolate flavour has topping options as chocochips :Rs 15,caramel :Rs 10,nuts :Rs 13 !\nChoose any one of the toppings mentioned above,Else type no : ")
-        topping = topping.lower()
-        return (getattr(ice_object,topping))*self.quantity
-            
+class IceCream:
+    def __init__(self,flavour,type,fcost,tcost,toppings):
+        self.flavour = flavour
+        self.type = type
+        self.fcost = fcost
+        self.tcost = tcost
+        self.toppings = toppings
+    def cost(self):
+        if self.flavour == "chocolate":
+            print("Chocolate flavour has toppings option :")
+            for topping,cost in self.toppings.items():
+                if topping != "no":
+                    print(topping,":","Rs",cost)
+            topping = input("\nChoose any one of the toppings mentioned above,Else type no : ")
+            topping = topping.lower()
+            return self.fcost + self.tcost + self.toppings[topping]
+        else:
+            return self.fcost + self.tcost
+        
+class IceCreamShop:
+    def __init__(self,flavours,types,toppings):
+        self.flavours =flavours
+        self.types = types
+        
+        for flavour,fcost in flavours.items():
+            for type,tcost in types.items():
+                print(flavour+" "+type," "*(18-len(flavour+type)),fcost+tcost)
+        
+        print("\nChoose your favourite ice-cream :",end=" ")
+        choice = input().split()
+        quantity = int(input("Please enter the quantity in number : "))
+        self.cost = IceCream(choice[0].lower(),choice[1].lower(),self.flavours[choice[0].lower()],self.types[choice[1].lower()],toppings)
+
+        self.quantity = quantity
+    def total_cost(self):
+            total = self.cost.cost()*self.quantity
+            return total
+
+
 
 print("*-*-*Welcome to Green-icecream stall*-*-*")
 print("*---------------MENU CARD---------------*\n")
-print("ICE CREAM          flavour+type    COST\nChocolate stick    40+5           Rs 45\nChocolate cone     40+10          Rs 50\nChocolate cup      40+15          Rs 55\nVanilla stick      20+5           Rs 25\nVanilla cone       20+10          Rs 30\nVanilla cup        20+15          Rs 35\nStrawberry stick   60+5           Rs 65\nStrawberry cone    60+10          Rs 70\nStrawberry cup     60+15          Rs 75")
-print("\nChoose your favourite ice-cream :",end=" ")
-choice = input().split()
-quantity = int(input("Please enter the quantity in number : "))
-ice_object = ChocolateIcecream(choice[0].lower(),choice[1].lower(),quantity)
-if choice[0].lower() != "chocolate":
-    print("The cost of ice-cream is : Rs",ice_object.cost())
-else:
-    print("The cost of ice-cream is : Rs",ice_object.cost()+ice_object.toppings())
+print("ICE CREAM          COST\n")
+
+flavours = {"vanilla" : 20,"strawberry" : 60,"chocolate" : 40}
+types = {"stick" : 5,"cone" : 10,"cup" :15}
+toppings ={"caramel" : 10,"choco chips" : 15,"nuts" : 13,"no" : 0}
+
+ice_object = IceCreamShop(flavours,types,toppings)
+print("The total cost is",ice_object.total_cost())
 
 """
-Output :
+Input and output :
 
 *-*-*Welcome to Green-icecream stall*-*-*
 *---------------MENU CARD---------------*
 
-ICE CREAM          flavour+type    COST
-Chocolate stick    40+5           Rs 45
-Chocolate cone     40+10          Rs 50
-Chocolate cup      40+15          Rs 55
-Vanilla stick      20+5           Rs 25
-Vanilla cone       20+10          Rs 30
-Vanilla cup        20+15          Rs 35
-Strawberry stick   60+5           Rs 65
-Strawberry cone    60+10          Rs 70
-Strawberry cup     60+15          Rs 75
+ICE CREAM          COST
+
+vanilla stick        25
+vanilla cone         30
+vanilla cup          35
+strawberry stick     65
+strawberry cone      70
+strawberry cup       75
+chocolate stick      45
+chocolate cone       50
+chocolate cup        55
 
 Choose your favourite ice-cream : chocolate cone
 Please enter the quantity in number : 3
+Chocolate flavour has toppings option :
+caramel : Rs 10
+choco chips : Rs 15
+nuts : Rs 13
 
-Chocolate flavour has topping options as chocochips :Rs 15,caramel :Rs 10,nuts :Rs 13 !
 Choose any one of the toppings mentioned above,Else type no : caramel
-The cost of ice-cream is : Rs 180
+The total cost is 180
 
 """
