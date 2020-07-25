e1c6259ec28903171338008208ea511fc3879bf0
Pachaiammal_k <pachai4477@gmail.com>
2020-07-25 19:49:17 +0530

    Updated GoRide registration program'

--- go_ride_registrations.py
+++ go_ride_registrations.py
@@ -1,10 +1,11 @@
-class Driver:
-    def __init__(self,name,age,driving_license_number,license_validity_period):
+import re
+class Owner:
+    def __init__(self,name,age,driving_license_number,license_validity_period,category,car_number,colour,company,model):
         self.name = name
         self.age = age
         self.driving_license_number = driving_license_number
         self.license_validity_period = license_validity_period
-
+        self.car_object = Car(category,car_number,colour,company,model)
     def display_confirmation(self):
         print("\nThanks for using GoRide,Registration Successful !")
 
@@ -16,34 +17,31 @@ class Car:
         self.company = company
         self.model = model
 
-    def display_confirmation(self):
-        print("\nThanks for using GoRide,Registration Successful !")
-
-def is_driver(selection):
-    if selection.lower() == "driver":
-        return True
-
-
 def registration():
-    print("Welcome to GoRide !\nDo you want to  register as driver or owner ?")
-    selection = input("select driver/owner :")
-    if is_driver(selection):
-        name = input("Enter your name : ")
-        age = input("Enter your age : ")
+    print("Welcome to GoRide !")
+    name = input("Enter your name : ")
+    age = input("Enter your age : ")
+    while True:
         driving_license_number = input("Enter your driving license number : ")
-        license_validity_period = input("Enter your license validity (dd - mm - yy) :")
-        driver_object = name+age
-        driver_object = Driver(name,age,driving_license_number,license_validity)
-        driver_object.display_confirmation()                                
-    else:
-        print("Select car category as micro- can accomodate upto 4 people / XL- can accomodate upto 10 people")
-        category = input("Enter your car category : ")
-        car_number = input("Enter your car number : ")
-        colour = input("Enter your car colour : ")
-        company = input("Enter your car company : ")
-        model = input("Enter your car model : ")
-        car_object = Car(category,car_number,colour,company,model)
-        car_object.display_confirmation()
+        if re.match(r"([A-Z]{2}[0-9]{2})((19|20)[0-9][0-9])([0-9]{7})",driving_license_number):
+            break
+        else:
+            print("please enter a valid license number")
+    while True:
+        license_validity_period = input("Enter your license validity (dd-mm-yyyy) : ")
+        if re.match(r"(0[1-9]|[12][0-9]|3[01])[-](0[1-9]|1[012])[-](202[1-9])",license_validity_period):
+            break
+        else:
+            print("please enter the valid date !")
+    print("Select car category as micro- can accomodate upto 4 people / XL- can accomodate upto 10 people ")
+    category = input("Enter your car category : ")
+    car_number = input("Enter your car number : ")
+    colour = input("Enter your car colour : ")
+    company = input("Enter your car company : ")
+    model = input("Enter your car model : ")
+    
+    owner_object = Owner(name,age,driving_license_number,license_validity_period,category,car_number,colour,company,model)
+    owner_object.display_confirmation()
         
 registration()
 
